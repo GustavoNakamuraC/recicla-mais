349be2cc9ab038bad2a9b94bcc44d859b673c1ba
JoaoVictorNas <joacrestani07@gmail.com>
2025-03-31 22:23:27 -0300

    feat/Ajustando a tela de cadastro

---
+++ recicla-mais/src/pages/Cadastro/Cadastro.tsx
@@ -0,0 +1,13 @@
+import './Cadastro.css'
+
+const Cadastro = () =>{
+    return(
+        <div className="tela">
+            <div className='container'>
+                <h1 className='tittle'>Recicla+</h1>
+            </div>
+        </div>
+    );
+}
+
+export default Cadastro;
